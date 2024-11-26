11154c87e5161ad7fbbfb347687274cb1641d4a5
Deepak Mishra <38439630+Deepak2698@users.noreply.github.com>
2024-11-27 01:59:45 +0530

    project

---
+++ src/InfluencerList.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import { fetchInfluencers, deleteInfluencer } from '../api';
+
+const InfluencerList = () => {
+  const [influencers, setInfluencers] = useState([]);
+
+  useEffect(() => {
+    const getInfluencers = async () => {
+      const { data } = await fetchInfluencers();
+      setInfluencers(data);
+    };
+    getInfluencers();
+  }, []);
+
+  const handleDelete = async (id) => {
+    await deleteInfluencer(id);
+    setInfluencers(influencers.filter((inf) => inf._id !== id));
+  };
+
+  return (
+    <div>
+      <h1>Influencer List</h1>
+      {influencers.map((inf) => (
+        <div key={inf._id}>
+          <h2>{inf.name}</h2>
+          <p>{inf.socialMedia.platform}: {inf.socialMedia.handle} ({inf.socialMedia.followers} followers)</p>
+          <button onClick={() => handleDelete(inf._id)}>Delete</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default InfluencerList;
